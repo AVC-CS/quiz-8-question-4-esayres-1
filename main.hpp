9554c167deaa3afd60ab8fea2bf63a129f3cc0db
Elijah Sayres <elijahsayres@gmail.comm>
2024-02-23 17:56:34 -0800

    First Commit: setting up file + starting on splitline() function

--- main.hpp
+++ main.hpp
@@ -8,6 +8,15 @@ const int MAXLEN = 100;
 
 int splitline(string, char, char[][MAXLEN]);
 
-/*******************************
- * Code your program here
- *******************************/
+int splitline(string txt, char delimiter, char array[][MAXLEN]){
+    int i = 0;
+    int j = 0;
+    int cnt = 0;
+
+
+    while(txt[i] != '\0'){
+        array[cnt][j] = txt[i];
+        j++;
+
+    }
+}
